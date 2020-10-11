ff987f1074b1ae66128db044ac6031f7f57ab4d7
mumler <mumler@mumlers.room-1.0>
2020-10-11 14:58:26 +0000

    simplepeer room signaling, changed turn/stun to xirsys

--- app_test.js
+++ app_test.js
@@ -1,11 +1,10 @@
+/*jshint esversion:6*/
 const fs = require('fs');
 const express = require('express');
 const httpolyglot = require('httpolyglot');
 const pug = require('pug');
 const path = require('path');
 let io = require('socket.io');
-//ech just checking instead og polyglot 
-// const https = require('https');
 
 const app = express();
 
@@ -37,18 +36,118 @@ httpsServer.listen(port, ()=>{
 	console.log('listening on port 8080 !');
 });
 
+// ⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤
+// ⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤
+// ⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤
+// !!! GETTING THE ICE 
+// !!! WHICH ISN'T WORKING BUT SHOULD, FOR NOW IM PROVIDING THE CREDENTIALS explicitly ON CLIENT SIDE  
+// let ice_str = "";
+// const https = require('https');
+// let o = {format:'urls'};
+// let bodyString = JSON.stringify(o);
+// let options = {
+//       host: "global.xirsys.net",
+//       path: "/_turn/mumler",
+//       method: "PUT",
+//       headers: {
+//           "Authorization": "Basic " + Buffer.from("czajniczek:ce004e12-0bbf-11eb-80b4-0242ac150002").toString("base64"),
+//           "Content-Type": "application/json",
+//           "Content-Length": bodyString.length
+//       }
+// };
+// let httpreq = https.request(options,res=>{
+// 	console.log('request made');
+// 	let str="";
+// 	res.on('data',data=>{
+// 		str+=data;
+// 		console.log(str);
+// 	});
+// 	res.on('error',e=>console.log('erros: ',e));
+// 	res.on('end',()=>{
+// 		ice_str=str;
+// 		console.log('response ended');
+
+// 	});
+// });
+// httpreq.on('error',e=>console.log('req error: ',e));
+// httpreq.end(console.log('request ended'));
+
+// ⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤
+// ⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤
+// ⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤⬤
+
 // io ~~~~~ passing peerID around (its just gonan be faster)
+//collection of all rooms , {roomId:[...],roomId:[...],roomId:[...]} 
+const rooms = {};
+//collection of all sockets, {socketId:roomId,socketId:roomId}
+const socketToRoom = {};
+
 io = io(httpsServer);
+
 io.on('connection', socket => {
-	console.log("socket connected:");
+	// console.debug('socket connected',socket);
 
-	socket.on('partnerEntered',()=>{
-		socket.broadcast.emit('partnerEntered');
+	socket.on('join room',name=>{
+		if(rooms[name]){ // if room already exists
+			const length = rooms[name].length;
+			if(length === 4){
+				socket.emit('room full');
+				return;		
+			} // ^^ is room full ?
+			rooms[name].push(socket.id); // << add to existing room's array
+		}else{
+			rooms[name]=[socket.id]; // create roomId : [creator] entry in rooms collection
+		}
+		socketToRoom[socket.id]=name; // add to sockets collection
+
+		const prevUser = getPrevUser(socket.id); // gettign adjacent users.
+		const nextUser = getNextUser(socket.id); // gettign adjacent users.
+		const usersInThisRoom = rooms[name].filter(id => id !== socket.id);
+		// socket.emit('ice config',ice_str);
+		socket.emit('all users',usersInThisRoom,prevUser,nextUser); //send room members to joiner (without joiner);
 	});
 
-	socket.on('signal',data=>{
-		socket.broadcast.emit('signal',data);
-	});
+	socket.on('sending signal',payload=>{ // emitted by joiner (createPeer fn), carries offer signal
+		const roomMember_id = payload.userToSignal;
+		const prevUser = getPrevUser(roomMember_id);
+		const nextUser = getNextUser(roomMember_id);
+		// io.to(roomMember_id).emit('ice config',ice_str);
+		io.to(roomMember_id).emit('user joined',{signal:payload.signal,callerId:payload.callerId},prevUser,nextUser); // give existin member offer signal and id it comes from
+	});  // ^^ called separately for each room member
+
+	socket.on('returning signal',payload=>{ //emitted by existign member (addPeer fn), carries answer signal
+		io.to(payload.callerId).emit('receiving returned signal',{signal:payload.signal,id:socket.id});
+	}); // ^^ sent to joiner.
+
+	socket.on('disconnect',()=>{ // ?? can i also emit this when leaving room ??
+		const roomId = socketToRoom[socket.id]; //get room socket belonged to
+		let room = rooms[roomId]; // get user array of that room 
+		if (room){
+			room = room.filter(id => id !== socket.id);
+			rooms[roomId] = room;
+			rooms[roomId].forEach(id=>{
+				const prevUser = getPrevUser(id);
+				const nextUser = getNextUser(id);
+				io.to(id).emit('user left',socket.id,prevUser,nextUser);
+			});
+		}		 
+	});// ^^update the room array, send everyone updated prevUser, nextuser
 
 });
 
+function getPrevUser(memberId){
+	const name = socketToRoom[memberId];
+	const member_i = rooms[name].indexOf(memberId);
+	let partner_i = member_i - 1;
+	partner_i = partner_i%rooms[name].length;
+	partner_i = partner_i < 0 ? partner_i + rooms[name].length : partner_i;
+	return rooms[name][partner_i]; 
+} // -> PrevUserId
+
+function getNextUser(memberId){
+	const name = socketToRoom[memberId];
+	const member_i = rooms[name].indexOf(memberId);
+	let partner_i = member_i + 1;
+	partner_i = partner_i%rooms[name].length;
+	return rooms[name][partner_i];
+} // -> NextUserId
--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 /*jshint esversion:6*/
 
-const socket = io();
-
-socket.on('connect',()=>{
-	console.log('socket connected');
-	socket.emit('partnerEntered');
-});
-
-navigator.getUserMedia = ( navigator.getUserMedia ||
-                       navigator.webkitGetUserMedia ||
-                       navigator.mozGetUserMedia ||
-                       navigator.msGetUserMedia);
+// navigator.getUserMedia = ( navigator.getUserMedia ||
+//                        navigator.webkitGetUserMedia ||
+//                        navigator.mozGetUserMedia ||
+//                        navigator.msGetUserMedia);
 
 const constraints = {
 	audio : false,
@@ -24,41 +17,256 @@ const constraints = {
 			ideal:320
 		},
 		resizeMode:"crop-and-scale"
-	}
+	}};
+
+const Peer = require('simple-peer');
+// let config  = {'iceServers': [
+// 				   { urls: ['stun:stun.l.google.com:19302']},
+// 				   { urls: ['turn:numb.viagenie.ca:3478'], //?transport=tcp
+// 				   	username: 's9lowacki@gmail.com',
+// 				    credential: 'testingtestint'}
+// 				    ]
+// 				};
+
+let config = {"iceServers" : [
+		{urls: [ "stun:eu-turn2.xirsys.com" ]},
+		{username: "DhZKBos2ZBjkR6rB4RzhDu6rSVpWIWDLUZJWQhBYDDVGBtb5qcdVUtGYow26onscAAAAAF-DEjZjemFqbmljemVr",
+	   	credential: "71ed90ba-0bcb-11eb-b26b-0242ac140004",
+	   	urls: [
+	       "turn:eu-turn2.xirsys.com:80?transport=udp",
+	       "turn:eu-turn2.xirsys.com:3478?transport=udp",
+	       "turn:eu-turn2.xirsys.com:80?transport=tcp",
+	       "turn:eu-turn2.xirsys.com:3478?transport=tcp",
+	       "turns:eu-turn2.xirsys.com:443?transport=tcp",
+	       "turns:eu-turn2.xirsys.com:5349?transport=tcp"
+	   	]}
+	]};
+
+let socket;
+const peersRef = []; // locally stored array of peers in the room 
+let partnerPrev;
+let partnerNext;
+window.getMembers = ()=>{
+	console.log(peersRef);
 };
 
-const config  = {'iceServers': [
-				   { url: 'stun:stun.l.google.com:19302' },
-				   { url: 'turn:numb.viagenie.ca?transport=udp',
-				   	username: 's9lowacki@gmail.com',
-				    credential: 'testingtestint'}
-			 	]};
+navigator.mediaDevices.getUserMedia(constraints).then(myStream =>{
 
-navigator.getUserMedia(constraints,(myStream)=>{
-	const Peer = require('simple-peer');
+	socket = io();
+
+	socket.on('connect',()=>{
+		console.log('socket connected');
+	});
+
+	// TRYING TO GET CONFIG VIA API, DOESNT WOOORKKK. 
+	// socket.on('ice config',ice_str=>{
+	// 	if(ice_str.length == 0){
+	// 		console.error('ice string is empty');
+	// 		return;
+	// 	}
+	// 	config = JSON.parse(ice_str);
+	// 	console.log(config);
+	// });
+
+	socket.emit('join room','test'); // server responds with 'all users'
+	// ^^ IN REAL LIFE THIS WOULD BE FIRED ON ROOM FORM.SUBMISSION  
+	socket.on('room full',()=>{
+		console.error('the room you are trying to join is full');		
+	});
+	socket.on('all users',(users,prevUser,nextUser)=>{ // < me joining ...
+		const peers = []; // this one is for media rendering on client side
+		users.forEach(userId=>{ // !! << skipped when 1 user in room (users array empty)
+			const peer = createPeer(userId,socket.id,myStream); // yourID myID mySTREAM
+			peersRef.push({
+				peerId:userId, //we make peer id same as socket id (remote peer, remote socket)
+				peer // < the actual peer object
+			});
+			peers.push(peer); 	// << this is for rendering client side			
+		});
+		setPeer(peers);		// << this is for rendering client side 
+	});
+
+	socket.on('user joined',(payload,prevUser,nextUser)=>{ // < i'm already in, someone else joins ... 
+		const peer = addPeer(payload.signal,payload.callerId,myStream); //signal object from remote, remote user id, my stream
+		peersRef.push({
+			peerId:payload.callerId,
+			peer
+		});
+	});
+
+	socket.on('receiving returned signal',payload=>{ // < JOINER returning signal :: answer signal
+		const item = peersRef.find(p=>p.peerId === payload.id); // finding the peer we are receiving an answer from
+		item.peer.signal(payload.signal);
+	});
+
+	socket.on('user left',(user,prevUser,NextUserId)=>{
+
+	});
+
+}).catch(err=>{
+	console.error(err);
+});
+
+//we need a peer not per user but per connection 
+function createPeer(userToSignal, callerId, stream){ // << called by joiner
 	const peer = new Peer({
-		initiator: location.hash === '#init',
-		reconnectTimer: 3000,
-      	// iceTransportPolicy: 'relay',
-		// trickle: false,
-		config: config,
-		stream: myStream
-	});	
+		config,
+		initiator:true, // << because the entering user needs to let other know she joined
+		trickle:false,
+		iceTransportPolicy: 'relay',
+		stream,  
+	});
+
+	peer.on('signal', signal=>{
+		console.debug('got the offer', signal);
+		socket.emit('sending signal', {userToSignal, callerId, signal}); // we send the signal to server & server passes the signal to specified remote user (via socket); 
+	});
+
+	peer.on('connect',()=>{
+		console.debug('peer connection is ready',peer);
+	});
+
+	peer.on('error',err=>console.error(err));
+	return peer;
+}
 
 
+function addPeer(incomingSignal, callerId, stream){ // << called by existing member
+	const peer = new Peer({
+		config,
+		initiator:false,
+		trickle:false,
+		iceTransportPolicy: 'relay',
+		stream,		
+	});
+	// 2. firing 'signal' event
+	peer.on('signal', signal=>{ // triggered when remote peer is signaling with offer, sending answer signal back
+		socket.emit('returning signal',{signal,callerId});
+		console.debug('got the answer', signal);
+	});
+	// 1. called first
+	peer.signal(incomingSignal); 
+
+	peer.on('connect',()=>{
+		console.debug('peer connection is ready',peer);
+	});
+
+	peer.on('error',err=>console.error(err));
+
+	return peer;
+}
+
+function setPeer(){
+	console.debug('called set peer fn');
+}
+
+/*
+flow:
+JOINER
+	1.
+	person joins room < initiator
+	initiator socket: 'im joining!'
+	io.server > sends array of room peers >to> init.socket 
+		arr of socket ids
+	for each socket id > create peer (with init)
+	init peer > signal event > get signal > socket: send  offer signal >to> remote peer
+	3.  
+	wait for answer signal from remote peer (via socket)
+	find peer the corresponding offer originated from 
+	call peer.singal on received answer 
+	4. 
+	CONNECTED !
+
+ALREADY IN
+	2.
+	socket "user joined!" with payload > signal obj and joiner id
+	create non-initiating peer
+	call .signal with offer received from joiner
+	get answer signal from 'signal event'
+	via socket : send answer signal >to> joiner	
+	4.
+	CONNECTED !
+*/
+
+
+//hmm, what happens when 1st user joins room ? 
+
+// hmm, when do we add new user to user arr stored on server ??
+// ^^ the array passed via 'all users' socket.event ?
+// aha, joiner's id is added by server to user arrau
+// ^^ but the server is passing it to joiner without joiner id (filtered).
+// solution: i need it to be sent with joiner, filter it out in 'all user' event callback.
+
+
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	
+/*
+	socket.on('startPeer',data=>{
+			console.log(data);
+			const peerId = data.peerId;
+			const peer = new Peer({
+				initiator: data.init,
+				reconnectTimer: 3000,
+		   		trickle: false,
+				config: config,
+				stream: myStream,
+				// iceTransportPolicy: 'relay',
+			});	
+
+			//fired by init;
+			peer.on('signal',signal_data=>{
+				console.log(signal_data);
+				socket.emit('signal',signal_data);
+			});
+			//fired by remote;
+			socket.on('signal',data=>{
+				console.log(data);
+				peer.signal(data.signal);
+			});
+
+			peer.on('connect',()=>{
+				console.log('peers connected');
+			});
+
+			peer.on('close',()=>{
+				console.log('connection closed');
+			});
+
+			peer.on('error', err=>{
+				console.log(err);
+			});
+	});
+
+}).catch(err=>{
+	console.error(err);
+});
+
+*/
+
+
+
+
+	/*
 	peer.on('signal', (data)=>{
 		//on peer signalled , returns peer id 
 		console.log('signal data: ', data);
 		waitForPeer(data);
 	});
 
-	function waitForPeer(data){
+	function waitForPeer(data){		
 		socket.on('partnerEntered',()=>{
+			console.log('other peer entered', data);
 			socket.emit('signal',data);
 		});
 	}
 
 	socket.on('signal',data=>{
+		// new peer signalling #init
 		peer.signal(data);
 	});
 
@@ -69,6 +277,7 @@ navigator.getUserMedia(constraints,(myStream)=>{
 	peer.on('error', err=>{
 		console.log('peer error: ',err);
 	});
+	*/
 
 
 	/*
@@ -104,6 +313,4 @@ navigator.getUserMedia(constraints,(myStream)=>{
 	*/
 
 
-},(err)=>{
-	console.log(err);
-});
+
